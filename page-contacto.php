27a42c518e20db02081eeb99ae503498b3f05cb5
fullloki <asbeljaen507@gmail.com>
2026-03-20 17:35:27 -0500

    acomodando css

--- page-contacto.php
+++ page-contacto.php
@@ -10,6 +10,28 @@
     Escribir por WhatsApp
   </a>
 
+  <div class="contact">
+    <div class="contact-form">
+      <form action="">
+        <label for="name">Nombre</label>
+        <input type="text" id="name" name="name" required>
+
+        <label for="email">Correo electrónico</label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="message">Mensaje</label>
+        <textarea id="message" name="message" rows="5" required></textarea>
+
+        <button type="submit" class="btn-primary">Enviar</button>
+      </form>
+    </div>
+
+   <div class="contact-info">
+      <h2>Información de contacto</h2>
+      <p><strong>Teléfono:</strong> +507 XXXXXXXX</p>
+      <p><strong>Email:</strong> info@clouditsoft.com</p>
+    </div>
+  </div>
 </section>
 
 <?php get_footer(); ?>
